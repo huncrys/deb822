33c410b9bca4efe0ae73c1653660ff31eb2439b3
Kristof Bach <crys@crys.hu>
2026-01-31 23:13:43 +0100

    docs: document new functions

--- rune_reader.go
+++ rune_reader.go
@@ -10,10 +10,12 @@ type RuneReader struct {
 	*bufio.Reader
 }
 
+// NewRuneReader creates a new RuneReader from the provided io.Reader.
 func NewRuneReader(r io.Reader) *RuneReader {
 	return &RuneReader{Reader: bufio.NewReader(r)}
 }
 
+// PeekRune peeks at the next rune without consuming it.
 func (r *RuneReader) PeekRune() (rune, int, error) {
 	rn, size, err := r.ReadRune()
 	if err != nil {
@@ -23,10 +25,12 @@ func (r *RuneReader) PeekRune() (rune, int, error) {
 	return rn, size, r.UnreadRune()
 }
 
+// DiscardRune discards the next rune.
 func (r *RuneReader) DiscardRune() {
 	_, _, _ = r.ReadRune()
 }
 
+// DiscardSpace discards all consecutive whitespace runes.
 func (r *RuneReader) DiscardSpace() {
 	for {
 		peek, _, err := r.ReadRune()
